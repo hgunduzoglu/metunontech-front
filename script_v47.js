2f40eb1d545da50cea45d873e613c65e5daca9da
Hüsamettin Gündüzoğlu <56267780+hgunduzoglu@users.noreply.github.com>
2025-07-02 15:53:32 +0300

    Update and rename script_v46.js to script_v47.js

--- script_v47.js
+++ script_v47.js
@@ -88,7 +88,7 @@ const TIME_SLOTS = [
 
 // Ana veri kaynağını (JSON) yükle ve sayfayı başlat
 function startApp() {
-  fetch("nte_time_with_codes_v7.json")
+  fetch("nte_time_with_codes_v8.json")
     .then(res => res.json())
     .then(data => {
       courseData = data;
