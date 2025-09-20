7aad064ac1ba9cd1f7d89c20dcec5b76e3bd1e49
Husam <e244617@metu.edu.tr>
2025-09-20 17:13:21 +0300

    speed insights

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Analytics } from '@vercel/analytics/react';
 import App from "./App";
+import { SpeedInsights } from "@vercel/speed-insights/react"
+
+
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
     <Analytics />
+    <SpeedInsights />
   </React.StrictMode>
 );
